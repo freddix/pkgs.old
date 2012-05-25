d273b6593fb354fb40caf0f05675e43336349b59
Fryderyk Dziarmagowski <freddy@freddix.org>
2012-05-25 12:10:24 +0200

    add untracked files

---
+++ firefox/vendor.js
@@ -0,0 +1,8 @@
+pref("app.update.autoInstallEnabled", false);
+pref("app.update.enabled", false);
+pref("browser.shell.checkDefaultBrowser", false);
+pref("extensions.autoDisableScopes", 11);
+pref("extensions.shownSelectionUI", true);
+pref("general.smoothScroll", true);
+pref("intl.locale.matchOS", true);
+pref("toolkit.storage.synchronous", 0);
